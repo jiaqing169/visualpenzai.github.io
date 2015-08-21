21d72a8aedf111a9e67ccbba1810caa6a940d428
Bharadwaj <bharadwaj.turlapati@kony.com>
2015-08-21 21:07:03 +0530

    Shop bday Celebrations

---
+++ js/app.js
@@ -0,0 +1,11 @@
+//js file for custom java script models.
+
+var fourthBday = function(){
+	vex.defaultOptions.className = 'vex-theme-os';
+	var todayDateString = "";
+	vex.dialog.alert({
+		message: 'SHOP Celebrates its 4th Birthday !! ',
+		//input: "<div class =\"Upload\"> <a href =\"#\" onclick=\"initUpload()\"> Upload Application Binary</a> </div>\n \n<div class =\"or\">  OR </div>\n \n<div class =\"Search\" > <a href =\"#\" onclick=\"initSearch()\"> Search from Google Playstore </a></div>\n",
+		className:'vex-theme-flat-attack'
+	});
+}
